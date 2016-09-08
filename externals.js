62495c4ee6c94e0bf75114cd91b3b72884b76d76
bigzhu <vermiliondun@gmail.com>
2016-09-08 16:26:29 +0800

    update name

--- externals.js
+++ externals.js
@@ -2,7 +2,7 @@ module.exports = {
   jQuery: 'jQuery',
   jquery: 'jquery',
   'bz-simditor': 'bz-simditor',
-  'vue-resource': 'VueResource',
+  'vue-resource': 'vue-resource',
   'bz-semantic-ui-form': 'bz-semantic-ui-form',
   'bz-upload-picture': 'bz-upload-picture',
   'toastr': 'toastr',
