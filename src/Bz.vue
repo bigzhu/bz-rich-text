ddca36db04c2d5782733b55e2ca4483ff3aa9810
bigzhu <vermiliondun@gmail.com>
2016-09-09 15:07:51 +0800

    支持编辑

--- src/Bz.vue
+++ src/Bz.vue
@@ -2,25 +2,25 @@
 </style>
 
 <template>
-<form v-on:submit.prevent="save" class="ui form">
-  <div class="field">
-    <label>标题图片</label>
-    <bz-upload-picture :img_src="title_img" :call_back="uploadPicture"></bz-upload-picture>
-  </div>
-  <div class="required field">
-    <label>标题</label>
-    <input v-model="title" type="text" name="title" placeholder="标题">
-  </div>
-  <div class="field">
-    <label>摘要</label>
-    <bz-simditor :content.sync="summary"></bz-simditor>
-  </div>
-  <div class="required field">
-    <label>内容</label>
-    <bz-simditor :content.sync="text"></bz-simditor>
-  </div>
-  <button class="ui button" type="submit">保存</button>
-</form>
+  <form v-on:submit.prevent="save" class="ui form">
+    <div class="field">
+      <label>标题图片</label>
+      <bz-upload-picture :img_src="title_img" :call_back="uploadPicture"></bz-upload-picture>
+    </div>
+    <div class="required field">
+      <label>标题</label>
+      <input v-model="title" type="text" name="title" placeholder="标题">
+    </div>
+    <div class="field">
+      <label>摘要</label>
+      <bz-simditor :content.sync="summary"></bz-simditor>
+    </div>
+    <div class="required field">
+      <label>内容</label>
+      <bz-simditor :content.sync="text"></bz-simditor>
+    </div>
+    <button class="ui button" type="submit">保存</button>
+  </form>
 </template>
 
 <script>
@@ -64,7 +64,8 @@
         required: true
       },
       id: {
-        type: Number
+        type: Number,
+        default: -1
       }
     },
     components: {
@@ -85,24 +86,37 @@
         if (this.title === '') throw new Error('请填入标题!')
         if (this.content === '') throw new Error('请填入内容!')
 
-        var parm = JSON.stringify(
-          {
-            title: this.title,
-            title_img: this.title_img,
-            summary: this.summary,
-            text: this.text,
-            key: this.key
-          }
-        )
-        api_rich_text.save(parm).then(
-          (response) => {
-            if (response.data.error !== '0') throw new Error(response.data.error)
-            if (this.save_call_back) this.save_call_back(this.title_img, this.title, this.summary, this.text, this.key)
-          },
-          (response) => {
-            toastr.error(response.status)
-          }
-        )
+        var parm = {
+          title: this.title,
+          title_img: this.title_img,
+          summary: this.summary,
+          text: this.text,
+          key: this.key
+        }
+        if (this.id === -1) {
+          parm = JSON.stringify(parm)
+          api_rich_text.save(parm).then(
+            (response) => {
+              if (response.data.error !== '0') throw new Error(response.data.error)
+              if (this.save_call_back) this.save_call_back(this.title_img, this.title, this.summary, this.text, this.key)
+            },
+            (response) => {
+              toastr.error(response.status)
+            }
+          )
+        } else {
+          parm.id = this.id
+          parm = JSON.stringify(parm)
+          api_rich_text.put(parm).then(
+            (response) => {
+              if (response.data.error !== '0') throw new Error(response.data.error)
+              if (this.save_call_back) this.save_call_back(this.title_img, this.title, this.summary, this.text, this.key)
+            },
+            (response) => {
+              toastr.error(response.status)
+            }
+          )
+        }
       }
     }
   }
